1d8618d353fe15d45819b3ea724dd0fb026ca742
ragaviramesh <ragavi.ramesh@gatech.edu>
2017-04-01 00:04:10 -0400

    developer page edited

--- web-app/super-mem/app/developer-mode/developer-mode.component.js
+++ web-app/super-mem/app/developer-mode/developer-mode.component.js
@@ -3,7 +3,9 @@ angular.module('developerMode').component('developerMode', {
 
     controller: ['$routeParams', '$route', '$firebaseObject', '$firebaseArray', function developerModeController($routeParams, $route, $firebaseObject, $firebaseArray) {
         var self = this;
-        var user = firebase.auth().currentUser;
-        self.developerModeRef = firebase.database().ref().child("users").child(user.uid).child("developer-mode");
+        //var user = firebase.auth().currentUser;
+        //self.developerModeRef = firebase.database().ref().child("users").child(user.uid).child("developer-mode");
+        self.memesRef = firebase.database().ref().child("memes");
+        self.memesArray = $firebaseArray(self.memesRef);
     }]
 });
