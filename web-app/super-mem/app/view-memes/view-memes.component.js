945a33da38c1000fbfe779a8aba7c6d8ee0ca7d4
deb <debkbanerji@gmail.com>
2017-03-31 21:13:41 -0400

    Successfully viewing unformatted memes

--- web-app/super-mem/app/view-memes/view-memes.component.js
+++ web-app/super-mem/app/view-memes/view-memes.component.js
@@ -3,7 +3,8 @@ angular.module('viewMemes').component('viewMemes', {
 
     controller: ['$routeParams', '$route', '$firebaseObject', '$firebaseArray', function viewMemesController($routeParams, $route, $firebaseObject, $firebaseArray) {
         var self = this;
-        var user = firebase.auth().currentUser;
-        self.viewMemesRef = firebase.database().ref().child("users").child(user.uid).child("view-memes");
+        // var user = firebase.auth().currentUser;
+        self.memesRef = firebase.database().ref().child("memes");
+        self.memesArray = $firebaseArray(self.memesRef);
     }]
 });
