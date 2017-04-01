d63ff4604985f7086f234f443e357fb7558ac094
ahirschberg <alexhirschberg97@gmail.com>
2017-04-01 15:28:16 -0400

    Basic text region recognition (2nd step after image recog)

--- cv/floodfill.py
+++ cv/floodfill.py
@@ -25,7 +25,9 @@ import os
 # hierarchy structure:
 # http://docs.opencv.org/3.1.0/d9/d8b/tutorial_py_contours_hierarchy.html
 CV_CONTOUR_PARENT = 2
+IMAGE_NORM_WIDTH = 500
 IMAGE_SUBCOMPONENT_THRESHOLD = 30 * 30 # if less than some value of pixels (in the normed img), throw it out
+COLOR_BLACK = (0,0,0)
 
 def ensure_dir(file_path):
     directory = os.path.dirname(file_path)
@@ -36,7 +38,7 @@ def make_blank_img(h, w):
     return np.zeros((h, w, 3), np.uint8)
 
 def get_normalized_dimensions(width, height):
-    return 250, int(250/width * height)
+    return IMAGE_NORM_WIDTH, int(IMAGE_NORM_WIDTH/width * height)
 
 def img_normalize_dimensions(img, width=-1, height=-1):
     new_dims = get_normalized_dimensions(width, height)
@@ -45,7 +47,11 @@ def img_normalize_dimensions(img, width=-1, height=-1):
 # based on http://stackoverflow.com/a/26445324
 def grab_rgb(image, rect):
     x, y, r_width, r_height = rect
-    return img[y:y+r_height, x:x+r_width]
+    return image[y:y+r_height, x:x+r_width]
+
+def put_fill_rect(mask, rect, fill):
+    x, y, r_width, r_height = rect
+    cv2.rectangle(mask, (x, y), (x+r_width,y+r_height), fill, -1)
 
 def transform_rect(rect, scale_hw):
     height_scale, width_scale = scale_hw
@@ -56,8 +62,24 @@ def transform_rect(rect, scale_hw):
     r_height = round(r_height * height_scale)
     return (x, y, r_width, r_height)
 
-class MemeDecomposer:
+def contours_to_rectangles(contour_rects):
+    for cnt in contour_rects:
+        yield cv2.boundingRect(cnt), cnt
 
+# via morphology blog post
+# http://www.danvk.org/2015/01/07/finding-blocks-of-text-in-an-image-using-python-opencv-and-numpy.html
+def dilate(ary, N, iterations, method=cv2.dilate):
+    """Dilate using an NxN '+' sign shape. ary is np.uint8."""
+    kernel = np.zeros((N,N), dtype=np.uint8)
+    kernel[(N-1)//2,:] = 1
+    dilated_image = method(ary // 255, kernel, iterations=iterations)
+
+    kernel = np.zeros((N,N), dtype=np.uint8)
+    kernel[:,(N-1)//2] = 1
+    dilated_image = method(dilated_image, kernel, iterations=iterations)
+    return dilated_image
+
+class MemeDecomposer:
     def __init__(self, img_raw, img_h, img_w):
         self.img_raw = img_raw
         scaled_w, scaled_h = get_normalized_dimensions(img_w, img_h)
@@ -67,33 +89,23 @@ class MemeDecomposer:
         self.flood_scale_factor_hw = (img_h / scaled_h, img_w / scaled_w)
 
 
-    # via morphology blog post
-    # http://www.danvk.org/2015/01/07/finding-blocks-of-text-in-an-image-using-python-opencv-and-numpy.html
-    def dilate(self, ary, N, iterations):
-        """Dilate using an NxN '+' sign shape. ary is np.uint8."""
-        kernel = np.zeros((N,N), dtype=np.uint8)
-        kernel[(N-1)//2,:] = 1
-        dilated_image = cv2.dilate(ary // 255, kernel, iterations=iterations)
-
-        kernel = np.zeros((N,N), dtype=np.uint8)
-        kernel[:,(N-1)//2] = 1
-        dilated_image = cv2.dilate(dilated_image, kernel, iterations=iterations)
-        return dilated_image
-
+    def get_working_image(self):
+        working_img, scaled_dims = img_normalize_dimensions(self.img_raw.copy(), width=self.img_w, height=self.img_h)
+        return working_img, scaled_dims
 
     def flood_find_regions(self, seed_pt, draw=False, mask=None, flood_data=(-1, -1, 0), canny_threshold_lo_hi=(-1, -1), n_dilation_iter=1):
         mask = mask or self.DEFAULT_MASK
+        working_img, scaled_dims = self.get_working_image()
+        working_img = cv2.bilateralFilter(working_img, 11, 17, 25)
         flood_lo, flood_hi, flood_flags = flood_data
         canny_threshold_lo, canny_threshold_hi = canny_threshold_lo_hi
-        working_img, scaled_dims = img_normalize_dimensions(self.img_raw.copy(), width=self.img_w, height=self.img_h)
-        working_img = cv2.bilateralFilter(working_img, 11, 17, 25)
         flooded = working_img.copy()
         mask[:] = 0
         cv2.floodFill(flooded, mask, seed_pt, (0, 255, 0), (flood_lo,)*3, (flood_hi,)*3, flood_flags)
         cv2.circle(flooded, seed_pt, 2, (0, 0, 255), -1)
         flood_region = working_img - flooded # todo use a better strategy here. thresholding?
         edges = cv2.Canny(flood_region, canny_threshold_lo, canny_threshold_hi, apertureSize=5)
-        dilated_img = self.dilate(edges, N=3, iterations=n_dilation_iter)
+        dilated_img = dilate(edges, N=3, iterations=n_dilation_iter)
         im2, contours, hierarchy = cv2.findContours(dilated_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         contour_drawing = make_blank_img(h, w)
         rectangle_contours = []
@@ -104,12 +116,14 @@ class MemeDecomposer:
             if len(approx) == 4:
                 if contour_meta[CV_CONTOUR_PARENT] != -1:
                     print('dropping contour because it is not innermost.')
-                    # leftovers.append(cnt)
                 else:
-                    rectangle_contours.append(approx)
+                    _, _, normed_w, normed_h = cv2.boundingRect(approx)
+                    if normed_w * normed_h < IMAGE_SUBCOMPONENT_THRESHOLD:
+                        print("skipping contour with dimensions %s x %s" % (normed_w, normed_h))
+                    else:
+                        rectangle_contours.append(approx)
             else:
                 leftovers.append(cnt)
-
 
         if draw:
             cv2.drawContours(contour_drawing, rectangle_contours, -1, (0,255,0), 3)
@@ -121,20 +135,28 @@ class MemeDecomposer:
 
     def extract_image_regions(self, flooded_image, rectangle_contours):
         imgs_extracted = []
-        for cnt in rectangle_contours:
-            normed_rect = cv2.boundingRect(cnt)
-            _, _, normed_w, normed_h = normed_rect
-            if normed_w * normed_h < IMAGE_SUBCOMPONENT_THRESHOLD:
-                print("skipping contour with dimensions %s x %s" % (normed_w, normed_h))
-                continue
-            else:
-                full_rect = transform_rect(normed_rect, self.flood_scale_factor_hw)
-                x, y, w, h = full_rect
-                blank = make_blank_img(h, w)
-                blank[:] = grab_rgb(self.img_raw, full_rect)
-                imgs_extracted.append((blank, full_rect))
+        ignored_regions = []
+        for normed_rect, cnt in contours_to_rectangles(rectangle_contours):
+            full_rect = transform_rect(normed_rect, self.flood_scale_factor_hw)
+            x, y, w, h = full_rect
+            blank = make_blank_img(h, w)
+            blank[:] = grab_rgb(self.img_raw, full_rect)
+            imgs_extracted.append((blank, full_rect))
 
-        return imgs_extracted
+        return imgs_extracted, ignored_regions
+
+    def find_text(self, rect_regions):
+        image, scaled_dims = self.get_working_image()
+        image = cv2.GaussianBlur(image, (13,13), 0)
+        mask = np.ones(image.shape[:2], dtype="uint8") * 255
+        for rect_region, cnt in contours_to_rectangles(rect_regions): put_fill_rect(mask, rect_region, COLOR_BLACK)
+        mask = dilate(mask, 3, 3, method=cv2.erode)
+        edges = cv2.Canny(image, 30, 50, apertureSize=5)
+        mask = cv2.bitwise_and(edges, edges, mask=mask)
+        mask = dilate(mask, 3, 5)
+        im2, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
+        cv2.imshow('edge', image)
 
 if __name__ == '__main__':
     import sys
@@ -166,18 +188,21 @@ if __name__ == '__main__':
         if fixed_range:
             flags |= cv2.FLOODFILL_FIXED_RANGE
 
-        contours, rect_regions, flooded_img = meme_decomposer \
-            .flood_find_regions(seed_pt, draw=True,
+        non_rect_contours, rect_regions, flooded_img = meme_decomposer \
+            .flood_find_regions(seed_pt, draw=False,
                                 flood_data=(lo, hi, flags),
                                 canny_threshold_lo_hi=(thrs1, thrs2),
                                 n_dilation_iter=dilation_iterations)
-        imgs = meme_decomposer.extract_image_regions(flooded_img, rect_regions)
+        imgs, ignored = meme_decomposer.extract_image_regions(flooded_img, rect_regions)
+        meme_decomposer.find_text(rect_regions)
+        cv2.drawContours(flooded_img, ignored, -1, (0,0,250), 3)
+        # cv2.imshow('edge', flooded_img)
 
         seq = iter(range(len(imgs)))
         for img_data, pose in imgs:
             # TODO right now it's setting each image as 0.webp, 1.webp and so on. These should be uploaded to firebase
             ensure_dir('generated_img_components/')
-            cv2.imwrite('generated_img_components/' + str(next(seq)) + '.webp', img_data, [cv2.IMWRITE_WEBP_QUALITY, 20])
+            # cv2.imwrite('generated_img_components/' + str(next(seq)) + '.webp', img_data, [cv2.IMWRITE_WEBP_QUALITY, 20])
 
             # TODO write the pose data to json
             x, y, w, h = pose
