e04f90ccb3902500f9b049cd6edeea98ef677b03
deb <debkbanerji@gmail.com>
2017-04-01 03:40:56 -0400

    Reading in .meme file data
    ...and doing nothing with it.

--- web-app/super-mem/app/upload-memes/upload-memes.component.js
+++ web-app/super-mem/app/upload-memes/upload-memes.component.js
@@ -1,9 +1,21 @@
 angular.module('uploadMemes').component('uploadMemes', {
     templateUrl: 'upload-memes/upload-memes.template.html',
 
-    controller: ['$routeParams', '$route', '$firebaseObject', '$firebaseArray', function uploadMemesController($routeParams, $route, $firebaseObject, $firebaseArray) {
+    controller: ['$scope', '$routeParams', '$route', '$firebaseObject', '$firebaseArray', function uploadMemesController($scope, $routeParams, $route, $firebaseObject, $firebaseArray) {
         var self = this;
         var user = firebase.auth().currentUser;
-        self.uploadMemesRef = firebase.database().ref().child("users").child(user.uid).child("upload-memes");
+        // self.uploadMemesRef = firebase.database().ref().child("users").child(user.uid).child("upload-memes");
+        self.memeFile = null;
+        self.rawMemeData = null;
+        $scope.fileNameChanged = function (ele) {
+            self.memeFile = ele.files[0];
+            console.log(self.memeFile);
+            var reader = new FileReader();
+            reader.onload = function(progressEvent){
+                self.rawMemeData = this.result;
+                console.log(self.rawMemeData);
+            };
+            reader.readAsText(self.memeFile);
+        }
     }]
 });
