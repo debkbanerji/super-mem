e3da846d1e30503febb442b62c53c0126295071f
ahirschberg <alexhirschberg97@gmail.com>
2017-04-01 01:06:44 -0400

    Break into class

--- cv/floodfill.py
+++ cv/floodfill.py
@@ -20,17 +20,51 @@ from __future__ import print_function
 import numpy as np
 import cv2
 
-# via morphology
-def dilate(ary, N, iterations):
-    """Dilate using an NxN '+' sign shape. ary is np.uint8."""
-    kernel = np.zeros((N,N), dtype=np.uint8)
-    kernel[(N-1)//2,:] = 1
-    dilated_image = cv2.dilate(ary // 255, kernel, iterations=iterations)
+class MemeDecomposer:
 
-    kernel = np.zeros((N,N), dtype=np.uint8)
-    kernel[:,(N-1)//2] = 1
-    dilated_image = cv2.dilate(dilated_image, kernel, iterations=iterations)
-    return dilated_image
+    def __init__(self, img_h, img_w):
+        self.DEFAULT_MASK = np.zeros((img_h+2, img_w+2), np.uint8)
+        self.img_h = img_h
+        self.img_w = img_w
+
+
+    # via morphology
+    def dilate(self, ary, N, iterations):
+        """Dilate using an NxN '+' sign shape. ary is np.uint8."""
+        kernel = np.zeros((N,N), dtype=np.uint8)
+        kernel[(N-1)//2,:] = 1
+        dilated_image = cv2.dilate(ary // 255, kernel, iterations=iterations)
+
+        kernel = np.zeros((N,N), dtype=np.uint8)
+        kernel[:,(N-1)//2] = 1
+        dilated_image = cv2.dilate(dilated_image, kernel, iterations=iterations)
+        return dilated_image
+
+    def flood_find_regions(self, img, seed_pt, draw=False, mask=None, flood_data=(-1, -1, 0), canny_threshold_lo_hi=(-1, -1), n_dilation_iter=1):
+        mask = mask or self.DEFAULT_MASK
+        flood_lo, flood_hi, flood_flags = flood_data
+        canny_threshold_lo, canny_threshold_hi = canny_threshold_lo_hi
+
+        flooded = img.copy()
+        mask[:] = 0
+        cv2.floodFill(flooded, mask, seed_pt, (0, 255, 0), (flood_lo,)*3, (flood_hi,)*3, flood_flags)
+        cv2.circle(flooded, seed_pt, 2, (0, 0, 255), -1)
+        flood_region = img - flooded # todo use a better strategy here. thresholding?
+        edges = cv2.Canny(flood_region, canny_threshold_lo, canny_threshold_hi, apertureSize=5)
+        dilated_img = self.dilate(edges, N=3, iterations=n_dilation_iter)
+        im2, contours, hierarchy = cv2.findContours(dilated_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contour_drawing = np.zeros((h, w, 3), np.uint8)
+        cv2.drawContours(contour_drawing, contours, -1, (0,255,0), 3)
+        for cnt in contours:
+            x,y,r_width,r_height = cv2.boundingRect(cnt)
+            cv2.rectangle(contour_drawing, (x,y),(x+r_width,y+r_height),(0,0,255),2)
+        if draw:
+            cv2.imshow('floodfill', flooded)
+            cv2.imshow('edge', contour_drawing)
+
+    def extract_image_regions(self, img, regions):
+        
+        pass
 
 if __name__ == '__main__':
     import sys
@@ -44,37 +78,26 @@ if __name__ == '__main__':
         sys.exit(1)
 
     h, w = img.shape[:2]
-    mask = np.zeros((h+2, w+2), np.uint8)
     seed_pt = None
-    fixed_range = True
+    fixed_range = False
     connectivity = 4
+    meme_decomposer = MemeDecomposer(h, w)
 
     def update(dummy=None):
         if seed_pt is None:
             cv2.imshow('floodfill', img)
             return
-        flooded = img.copy()
-        mask[:] = 0
         lo = cv2.getTrackbarPos('lo', 'floodfill')
         hi = cv2.getTrackbarPos('hi', 'floodfill')
         thrs1 = cv2.getTrackbarPos('thrs1', 'edge')
         thrs2 = cv2.getTrackbarPos('thrs2', 'edge')
+        dilation_iterations = cv2.getTrackbarPos('dilation_iterations', 'edge')
         flags = connectivity
         if fixed_range:
             flags |= cv2.FLOODFILL_FIXED_RANGE
-        cv2.floodFill(flooded, mask, seed_pt, (0, 255, 0), (lo,)*3, (hi,)*3, flags)
-        cv2.circle(flooded, seed_pt, 2, (0, 0, 255), -1)
-        flood_region = img - flooded
-        edge = cv2.Canny(flood_region, thrs1, thrs2, apertureSize=5)
-        dilated_img = dilate(edge, N=3, iterations=5)
-        im2, contours, hierarchy = cv2.findContours(dilated_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cv2.imshow('floodfill', flooded)
-        copy_flooded = np.zeros((h, w, 3), np.uint8)
-        cv2.drawContours(copy_flooded, contours, -1, (0,255,0), 3)
-        for cnt in contours:
-            x,y,r_width,r_height = cv2.boundingRect(cnt)
-            cv2.rectangle(copy_flooded, (x,y),(x+r_width,y+r_height),(0,0,255),2)
-        cv2.imshow('edge', copy_flooded)
+        meme_decomposer.flood_find_regions(img, seed_pt, draw=True, flood_data=(lo,hi, flags), canny_threshold_lo_hi=(thrs1, thrs2), n_dilation_iter=dilation_iterations)
+
+
 
     def onmouse(event, x, y, flags, param):
         global seed_pt
@@ -88,6 +111,7 @@ if __name__ == '__main__':
     cv2.createTrackbar('hi', 'floodfill', 20, 255, update)
     cv2.createTrackbar('thrs1', 'edge', 2000, 5000, update)
     cv2.createTrackbar('thrs2', 'edge', 4000, 5000, update)
+    cv2.createTrackbar('dilation_iterations', 'edge', 3, 10, update)
 
     while True:
         ch = cv2.waitKey()
